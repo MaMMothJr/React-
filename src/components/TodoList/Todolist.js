93ebfba82916751f6a5cde70e3ff45ff0f573f7d
MaMMoth Jr <rskoptev@gmail.com>
2020-02-29 22:49:21 +0200

    2020-02-29

--- src/components/TodoList/Todolist.js
+++ src/components/TodoList/Todolist.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import TodoListItem  from '../TodoListItem/TodoListItem'
+import React, {useState, useEffect} from 'react';
+import TodoListItem  from '../TodoListItem/TodoListItem';
 
 function TodoList () {
+
+  const [tasks, setTasks] = useState([]);
+  fetch('http://localhost:3004/posts')
+    .then(response => response.text())
+    .then(text =>  setTasks(text));
+
+    console.log(tasks);
+
   return (
     <div>
-      <TodoListItem  />
-      <TodoListItem  />
-      <TodoListItem  />
-      <TodoListItem  />
-      <TodoListItem  />
+      {tasks.map(item => (
+        <TodoListItem key={item.id} title={item.title}/>
+      ))}
     </div>
   );
 }
