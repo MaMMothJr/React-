04669351266e400d229ee4297eb7258f4da95300
MaMMoth Jr <rskoptev@gmail.com>
2020-03-04 21:49:10 +0200

    2020-02-04

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,19 +4,36 @@ import TodoList from '../TodoList/TodoList';
 function Main () {
 
   const [tasks, setTasks] = useState([]);
+  const [newValueTodo, setNewValueTodo] = useState('');
+  const [newTodo, setNewTodo] = useState([]);
 
   useEffect(() => {
     fetch('http://localhost:3004/posts')
       .then(response => response.json())
       .then(text =>  setTasks(text))
-    },[])
+    },[newValueTodo])
 
-  console.log(tasks); //проверка
+  const addToTodo = (event) => {
+    if (event.key === 'Enter')  {
+        setNewTodo(
+          fetch('http://localhost:3004/posts', {
+            method:'POST',
+            body: JSON.stringify({title: newTodo}),
+            }
+          )
+        )
+    }
+    console.log('wrong key');
+  }
 
   return (
     <div>
       <div className="input-area">
-        <input placeholder="What shall i do today?" />
+        <input
+          placeholder="What shall i do today?"
+          type="text"
+          onChange={(event) => setNewValueTodo(event.target.value)}
+          onKeyPress={addToTodo} />
         <button>+</button>
       </div>
       <div className="h2-main">
